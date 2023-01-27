3f58a17bf6b1c5d3a9dc5c613363ab820cb300ed
sanenchen <2115707702@qq.com>
2023-01-27 11:34:08 +0800

    build

--- builddev.sh
+++ builddev.sh
@@ -2,9 +2,9 @@ appname="SHChat"
 rm -rf .git/
 xgo -targets=linux/amd64,linux/arm64,windows/amd64,darwin/amd64 -out "$appName" -ldflags="$ldflags" -tags=jsoniter .
 mkdir -p "dist"
-mv $appname-* dist
+mv $appname* dist
 cd dist
-upx -9 ./$appname-linux*
-upx -9 ./$appname-windows*
+upx -9 ./$appname*-linux*
+upx -9 ./$appname*-windows*
 find . -type f -print0 | xargs -0 md5sum >md5.txt
 cat md5.txt
