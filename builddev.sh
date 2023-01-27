6e93453c4b26b55d0dbb8124eeb3d9c59d4e093b
sanenchen <2115707702@qq.com>
2023-01-27 11:16:01 +0800

    actions run when push

---
+++ builddev.sh
@@ -0,0 +1,10 @@
+appname="SHChat"
+rm -rf .git/
+xgo -targets=linux/amd64,windows/amd64,darwin/amd64 -out "$appName" -ldflags="$ldflags" -tags=jsoniter .
+mkdir -p "dist"
+mv alist-* dist
+cd dist
+upx -9 ./alist-linux*
+upx -9 ./alist-windows*
+find . -type f -print0 | xargs -0 md5sum >md5.txt
+cat md5.txt
